78425a8fb913fcece575b76af993df243297bddd
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-02-22 19:53:00 -0300

    Feat: add abstract credit card class

--- Product/CreditCard.cs
+++ Product/CreditCard.cs
@@ -2,6 +2,8 @@ namespace Product
 {
     public class CreditCard
     {
-        
+        public string CardType { get; set; } = string.Empty;
+        public int CreditLimit { get; set; } = 0;
+        public int AditionalPayment { get; set; } = 0;
     }
 }
